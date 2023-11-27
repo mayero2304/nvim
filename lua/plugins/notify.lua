4570eca8eaebda6677cf3277fe700bca2679c260
mauricio <mauricio@pricode.io>
2023-11-27 11:39:28 -0500

    snippets vscode

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,20 +1,6 @@
 return {
   "rcarriga/nvim-notify",
-  enabled = false,
-  keys = {
-    {
-      "<leader>un",
-      function()
-        require("notify").dismiss({ silent = true, pending = true })
-      end,
-      desc = "Dismiss all Notifications",
-    },
+  opts = {
+    timeout = 6000,
   },
-  config = function()
-    local notify = require("notify")
-    -- this for transparency
-    notify.setup({ background_colour = "#000000" })
-    -- this overwrites the vim notify function
-    vim.notify = notify.notify
-  end,
 }
